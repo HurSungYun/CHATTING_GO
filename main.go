18b461296e23b5d4b2956a89cb2ce89c9f1f0f0a
Ethan Hur <ethan0311@gmail.com>
2017-02-24 11:31:49 +0900

    server push 예제 성공

--- main.go
+++ main.go
@@ -1,12 +1,10 @@
 package main
 
 import (
+	"fmt"
+	"time"
 	"log"
 	"net/http"
-	"os"
-	"time"
-
-	"golang.org/x/net/http2"
 )
 
 type User struct {
@@ -28,16 +26,36 @@ type Chatroom struct {
 	numOfMsg int
 }
 
-func main() {
-	cwd, err := os.Getwd()
-	if err != nil {
-		log.Fatal(err)
-	}
+const mainJS = `console.log("hello world");`
 
-	srv := &http.Server{
-		Addr:    ":8000", // Normally ":443"
-		Handler: http.FileServer(http.Dir(cwd)),
-	}
-	http2.ConfigureServer(srv, &http2.Server{})
-	log.Fatal(srv.ListenAndServeTLS("server.crt", "server.key"))
+const indexHTML = `<html>
+<head>
+	<title>Hello</title>
+		<script src="/main.js"></script>
+		</head>
+		<body>
+		</body>
+		</html>
+`
+
+func main() {
+	http.HandleFunc("/main.js", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, mainJS)
+	})
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path != "/" {
+			http.NotFound(w, r)
+			return
+		}
+		pusher, ok := w.(http.Pusher)
+		if ok { // Push is supported. Try pushing rather than waiting for the browser.
+			if err := pusher.Push("/main.js", nil); err != nil {
+				log.Printf("Failed to push: %v", err)
+			}
+		}
+		fmt.Fprintf(w, indexHTML)
+	})
+	// Run crypto/tls/generate_cert.go to generate cert.pem and key.pem.
+	// See https://golang.org/src/crypto/tls/generate_cert.go
+	log.Fatal(http.ListenAndServeTLS(":7072", "cert.pem", "key.pem", nil))
 }
